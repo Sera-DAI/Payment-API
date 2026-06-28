4390a6c81bf4494b054bbf46d398292049845dc0
Jhonatan <alexmercer20020@gmail.com>
2026-06-28 01:20:12 -0300

    Add Payment model to define payment-related data structure

---
+++ src/app/models.py
@@ -0,0 +1,13 @@
+from app.extensions import db, login_manager
+from dataclasses import dataclass
+
+@dataclass
+class Payment(db.Model):
+    __tablename__ = 'payments'
+    id: int = db.Column(db.Integer, primary_key=True)
+    value: float = db.Column(db.Float)
+    paid: bool = db.Column(db.Boolean, default=False)
+    bank_payment_id: int = db.Column(db.Integer, nullable=True)
+    qr_code: str = db.Column(db.String(100), nullable=True)
+    expiration_date = db.Column(db.DateTime)
+    
