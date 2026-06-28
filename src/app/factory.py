898b1339cd1bedcf0e7db1c9dc6b8a28865d1e82
Jhonatan <alexmercer20020@gmail.com>
2026-06-28 01:20:06 -0300

    Add factory module to create Flask application with custom JSON provider

---
+++ src/app/factory.py
@@ -0,0 +1,43 @@
+import os
+from flask import Flask
+from flask.json.provider import DefaultJSONProvider
+from werkzeug.local import LocalProxy
+from dataclasses import is_dataclass, asdict
+from app.extensions import db, login_manager
+
+
+class CustomJSONProvider(DefaultJSONProvider):
+    def default(self, o):
+        if isinstance(o, LocalProxy):
+            o = o._get_current_object()
+            
+        if is_dataclass(o):
+            return asdict(o)
+        
+        return super().default(o)
+    
+def create_app():
+    app = Flask(__name__)
+    app.json = CustomJSONProvider(app)
+    
+    db_user = os.getenv('MYSQL_USER')
+    db_password = os.getenv('MYSQL_PASSWORD')
+    db_host = os.getenv('DB_HOST')
+    db_name = os.getenv('MYSQL_DATABASE')
+    secret_key = os.getenv('SECRET_KEY')
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{db_user}:{db_password}@{db_user}/{db_name}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = secret_key
+    
+    db.init_app(app)
+    login_manager.init_app(app)
+    
+    from app.payments.mold import payments_bp
+    app.register_blueprint(payments_bp)
+    
+    with app.app_context():
+        from app import models
+        db.create_all()
+    
+    return app
