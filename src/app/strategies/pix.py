514525182f0b3a3dd71ed50315047549bbceceac
Jhonatan <alexmercer20020@gmail.com>
2026-06-28 22:57:43 -0300

    Add Pix payment strategy implementation

---
+++ src/app/strategies/pix.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+import uuid
+import qrcode 
+
+@dataclass
+class Pix:
+    def create_payment(self):
+        bank_payment_id = str(uuid.uuid4())
+        hash_payment = f'hash_payment_{str(uuid.uuid4())}'
+        img = qrcode.make(hash_payment)
+        img.save(f'src/app/static/img/qrcode/qr_code_payment_{str(uuid.uuid4())}.png') # type: ignore
+        
+        return {
+            "bank_payment_id": bank_payment_id,
+            "qr_code_path": f"qr_code_payment_{str(uuid.uuid4())}"
+        }
