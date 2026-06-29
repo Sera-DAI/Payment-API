f0810fbb023dee5692d396ad8ad33d98c837c027
Jhonatan <alexmercer20020@gmail.com>
2026-06-28 22:57:48 -0300

    Remove unused Pix payment strategy implementation

--- src/app/payments/strategies/pix.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Pix:
-    def create_payment(self):
-        return {
-            "bank_payment_id": "",
-            "qr_code_path": ""
-        }
