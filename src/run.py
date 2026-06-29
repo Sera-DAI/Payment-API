71fd55b88d32ea37099f63d3e0493c83264b3dbc
Jhonatan <alexmercer20020@gmail.com>
2026-06-29 02:24:31 -0300

    Update run.py to use socketio for running the application

--- src/run.py
+++ src/run.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
-from app.factory import create_app
+from app.factory import create_app, socketio
 
 load_dotenv()
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
