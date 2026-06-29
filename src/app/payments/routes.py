fc78697b244667b2217b4cd15a7f6168610fc4cb
Jhonatan <alexmercer20020@gmail.com>
2026-06-28 22:58:21 -0300

    Refactor create_payment_pix to instantiate Pix and set bank_payment_id and qr_code from the payment data

--- src/app/payments/routes.py
+++ src/app/payments/routes.py
@@ -3,6 +3,7 @@ from app.payments.schemas import payments_bp
 from datetime import datetime, timedelta
 from app.models import Payment
 from app.extensions import db
+from app.strategies.pix import Pix
 
 @payments_bp.route('/pix', methods=['POST'])
 def create_payment_pix():
@@ -15,6 +16,11 @@ def create_payment_pix():
         
     expiration_date = datetime.now() + timedelta(minutes=30)
     new_payment = Payment(value=data['value'], expiration_date=expiration_date)
+    pix_obj = Pix()
+    data_payment_pix = pix_obj.create_payment()
+    new_payment.bank_payment_id = data_payment_pix['bank_payment_id'] 
+    new_payment.qr_code = data_payment_pix['qr_code_path']
+    
     db.session.add(new_payment)
     db.session.commit()
     
