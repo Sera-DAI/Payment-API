d3437a690d9065986ed6c3e60b70c881fcdf543d
Jhonatan <alexmercer20020@gmail.com>
2026-06-28 22:58:13 -0300

    Update Payment model to change bank_payment_id type to string and ensure qr_code length is consistent

--- src/app/models.py
+++ src/app/models.py
@@ -8,7 +8,7 @@ class Payment(db.Model):
     id: int = db.Column(db.Integer, primary_key=True)
     value: float = db.Column(db.Float)
     paid: bool = db.Column(db.Boolean, default=False)
-    bank_payment_id: int = db.Column(db.Integer, nullable=True)
-    qr_code: str = db.Column(db.String(100), nullable=True)
+    bank_payment_id: str = db.Column(db.String(200), nullable=True)
+    qr_code: str = db.Column(db.String(200), nullable=True)
     expiration_date: datetime = db.Column(db.DateTime)
     
